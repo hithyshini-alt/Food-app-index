de7e79006c729dc78e9d57f62f7f2e03d1384508
hyma645 <145032227+hyma645@users.noreply.github.com>
2024-10-12 20:54:43 +0530

    index

---
+++ index.js
@@ -0,0 +1,38 @@
+const express = require('express');
+const cors = require('cors');
+const stripe = require('stripe')("sk_test_51Q5TmSECmBLhb4FSYK5Y4ZcMyWTlygIkR6k1DexC3aXcURRCZtln5v8CvWGcFXIBhmfAa2KCsQliYwIs4mA6Et8Y00pVKL92uX");
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+app.post('/create-checkout', async (req, res) => {
+  const { items } = req.body;
+
+  const line_items = items.map(item => ({
+    price_data: {
+      currency: 'inr',
+      product_data: { name: item.name },
+      unit_amount: item.price * 100,
+    },
+    quantity: item.quantity,
+  }));
+//https://boisterous-crepe-a0bc3e.netlify.app
+  try {
+    const session = await stripe.checkout.sessions.create({
+      payment_method_types: ['card'],
+      line_items,
+      mode: 'payment',
+      success_url: 'http://localhost:5173/success',
+      cancel_url: 'http://localhost:5173/failure',
+    });
+
+    res.json({ id: session.id });
+  } catch (error) {
+    console.error('Error creating checkout session:', error);
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
+});
+
+app.listen(4982, () => {
+  console.log('Server running on port 4982');
+});
